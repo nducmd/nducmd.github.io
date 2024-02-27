1b898ae263150f1d8fdcf6394f1593854060c3b2
nducmd <110773590+nducmd@users.noreply.github.com>
2024-02-27 12:39:47 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,56 @@ function toggleRegister() {
     document.querySelector('.login-nav__item.active').classList.remove('active');
     document.querySelector('.login-nav__item:nth-child(2)').classList.add('active');
 }
+
+function validateForm() {
+    // Fetching values from inputs
+    var username = document.getElementById('username').value.trim();
+    var password = document.getElementById('password').value;
+    var rePassword = document.getElementById('rePassword').value;
+    var email = document.getElementById('email').value.trim();
+    // console.log(username);
+    // console.log(password);
+    // console.log(rePassword);
+    // console.log(email);
+  
+    // Resetting previous error messages
+    document.getElementById('usernameError').textContent = "";
+    document.getElementById('passwordError').textContent = "";
+    document.getElementById('rePasswordError').textContent = "";
+    document.getElementById('emailError').textContent = "";
+  
+    // Validating username
+    if (username === "") {
+      document.getElementById('usernameError').textContent = "Username is required";
+      //return ;
+    }
+  
+    // Validating password
+    if (password === "") {
+      document.getElementById('passwordError').textContent = "Password is required";
+      //return ;
+    }
+  
+    // Validating password match
+    if (password !== rePassword) {
+      document.getElementById('rePasswordError').textContent = "Passwords do not match";
+      //return ;
+    }
+  
+    // Validating email
+    if (email === "") {
+      document.getElementById('emailError').textContent = "Email is required";
+      //return ;
+    } else if (!isValidEmail(email)) {
+      document.getElementById('emailError').textContent = "Invalid email format";
+      //return ;
+    }
+  
+    //return ; // Form submission allowed
+  }
+  
+  function isValidEmail(email) {
+    // Basic email format validation
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
